5a8c80cb2042482a804946968c99109a69739262
Christian Dinato <christiandinato@MAC.station>
2026-06-08 18:34:51 +0200

    Update Austrian ducat coin photos

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const STATIC_CACHE = "oroactive-static-v20260608-coin-card-layout-1";
+const STATIC_CACHE = "oroactive-static-v20260608-ducato-images-1";
 const STATIC_ASSETS = [
   "/index.html",
-  "/styles.css?v=20260608-coin-card-layout-1",
-  "/app.js?v=20260608-coin-card-layout-1",
+  "/styles.css?v=20260608-ducato-images-1",
+  "/app.js?v=20260608-ducato-images-1",
   "/manifest.json",
   "/manifest.webmanifest",
   "/oroactive-logo.png",
