f1b480f6cd9ebb811811bedcbddd1e894a348e92
oroactive <christian.dinato@gmail.com>
2026-07-02 17:00:37 +0200

    Add Australian Kangaroo 25 Dollar FDC coin

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_ID = "20260702-kangaroo-fdc-1";
+const BUILD_ID = "20260702-kangaroo-25-fdc-1";
 const CACHE_NAME = `oroactive-cache-${BUILD_ID}`;
 const LEGACY_CACHE_PREFIXES = ["oroactive-", "oroactive-cache-", "oroactive-static-", "oroactive-assets-", "static-", "asset-", "pwa-"];
 const NEVER_CACHE_PREFIXES = [
