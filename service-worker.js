d7f7b0ed3c1ec4a0a2954fede6c5ce4cc182d388
oroactive <christian.dinato@gmail.com>
2026-07-02 19:06:14 +0200

    Add Austria Philharmonic 10 Euro coin

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_ID = "20260702-austria-philharmonic-25-euro-1";
+const BUILD_ID = "20260702-austria-philharmonic-10-euro-1";
 const CACHE_NAME = `oroactive-cache-${BUILD_ID}`;
 const LEGACY_CACHE_PREFIXES = ["oroactive-", "oroactive-cache-", "oroactive-static-", "oroactive-assets-", "static-", "asset-", "pwa-"];
 const NEVER_CACHE_PREFIXES = [
