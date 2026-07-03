0ee669df252d7370f506813558c80cf6313a07b3
oroactive <christian.dinato@gmail.com>
2026-07-03 14:05:52 +0200

    Fix missing coin photos in catalog

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_ID = "20260702-austria-4-euro-fdc-1";
+const BUILD_ID = "20260703-coin-photos-1";
 const CACHE_NAME = `oroactive-cache-${BUILD_ID}`;
 const LEGACY_CACHE_PREFIXES = ["oroactive-", "oroactive-cache-", "oroactive-static-", "oroactive-assets-", "static-", "asset-", "pwa-"];
 const NEVER_CACHE_PREFIXES = [
